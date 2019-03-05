7bde19d2f1b707db3f9c3895e9cab7e2c2456c4e
Cordell Appel <cordell.appel@umontana.edu>
2019-03-04 20:15:27 -0700

    All Views Moved Over

---
+++ InsideScoopMobile/EditUserProfile.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InsideScoopMobile
+{
+    public partial class EditUserProfile : ContentPage
+    {
+        public EditUserProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InsideScoopMobile/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InsideScoopMobile
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void Login_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void SignUp_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
